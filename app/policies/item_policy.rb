83c874047e3b82ee27b398683757d2398e240101
Jessica Nash <jeenash2@gmail.com>
2022-07-20 21:05:36 +0900

    pundit implemented in items, functional

---
+++ app/policies/item_policy.rb
@@ -0,0 +1,37 @@
+class ItemPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      # ^^^^^^^ Will be called for index action
+      scope.all
+      # scope.where(user: user) # <- only user that created restaurant can see it
+    end
+  end
+
+  def create?
+    return true
+  end
+
+  def show?
+    return true
+  end
+
+  def edit?
+    # If the user is the own of the item => true; otherwise => false
+    ## user => current_user
+    ## record => @item (arg passed to authorize)
+
+    user == record.user
+  end
+
+  def destroy?
+    record.user == user
+    # user_is_owner_or_admin?
+  end
+
+  # private
+
+  # def user_is_owner_or_admin?
+  #   record.user == user || user.admin
+  # end
+end
