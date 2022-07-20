526f9b95a0aedc14aafd1cd8376c7373a34a043d
Jessica Nash <jeenash2@gmail.com>
2022-07-20 20:21:13 +0900

    items controller and views created with basic structure

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { in: 6..20 }
+  validates :details, presence: true, length: { maximum: 200 }
+  validates :category, presence: true
+  validates :daily_rate, presence: true
+  validates :daily_rate, numericality: { greater_than_or_equal_to: 100, less_than_or_equal_to: 100_000 }
 end
