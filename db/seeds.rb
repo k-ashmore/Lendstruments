759366c59c2dc6be221e89da60600039e5643433
Tyler Hennig <tylerjhennig@gmail.com>
2022-07-20 09:31:00 +0900

    made a seed file that puts all of us into the db for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cleaning database..."
+User.destroy_all
+
+puts "Seeding database with Plutonians..."
+password = "123456"
+
+ken = User.create(email: 'ken@gmail.com', password: password, reset_password_token: "ken", username: 'Ken')
+
+jessica = User.create(email: "jessica@gmail.com", password: password, reset_password_token: "jessica", username: 'Jess')
+
+tyler = User.create(email: 'tyler@gmail.com', password: password, reset_password_token: "tyler", username: 'tyler')
+
+nicole = User.create(email: 'nicole@gmail.com', password: password, reset_password_token: "nicole", username: 'nicole')
+
+puts "saving users..."
+ken.save
+jessica.save
+tyler.save
+nicole.save
+
+puts "done!"
