4cde1776ea8cd6881aec1671166ac5bea63b25ca
Nicole Olliver <noliver.ykc@gmail.com>
2022-07-23 13:04:17 +0900

    fixed ookings viewing error

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,6 @@
 class BookingsController < ApplicationController
   # code in silo so as you make a new page make a new method
+  def index
+    @bookings = policy_scope(Booking).order(created_at: :desc)
+  end
 end
