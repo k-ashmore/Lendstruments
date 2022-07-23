c8d869bf6af81b5c2c2e30cd1c3aac655de74a75
Jessica Nash <jeenash2@gmail.com>
2022-07-23 12:23:24 +0900

    bookings new and create actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,26 @@
 class BookingsController < ApplicationController
   # code in silo so as you make a new page make a new method
+  def new
+    @booking = Booking.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @item = Item.find(params[:item_id])
+    @booking.item = @item
+
+    if @booking.save
+      redirect_to @booking, notice: 'Booking request was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
 end
