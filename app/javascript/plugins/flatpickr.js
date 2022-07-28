98023856b65692efff0bae4dd1c53c920e41039d
noliver-ykc <74501096+noliver-ykc@users.noreply.github.com>
2022-07-28 22:34:43 +0900

    Update flatpickr.js

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,9 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  flatpickr(".datepicker", {
+    altInput: true
+  });
 }
 
 export { initFlatpickr };
